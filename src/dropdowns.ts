e86adf2a28ed1a083df4185c2ef3bca6155f6b52
Aleksandr <aleksandr.kirakosyan11@gmail.com>
2024-02-13 12:47:50 +0400

    new updates

---
+++ src/dropdowns.ts
@@ -0,0 +1,83 @@
+export const writingOptions = [
+    {
+        id: 1,
+        value: "Conversational",
+        description:
+            "Uses a friendly, casual tone that makes the reader feel like you're speaking directly to them. This approach helps build rapport and trust with potential customers.",
+    },
+    {
+        id: 2,
+        value: "Professional",
+        description:
+            "Maintains a polished and formal tone that conveys expertise and credibility. This style is suitable for high-end or technical products where professionalism is crucial.",
+    },
+    {
+        id: 3,
+        value: "Informative",
+        description:
+            "Provides clear, concise information about the product's features, benefits, and specifications to help readers make informed purchasing decisions.",
+    },
+    {
+        id: 4,
+        value: "Storytelling",
+        description:
+            "Creates a narrative around the product, highlighting its origin, the people who created it, or the problem it solves to engage readers emotionally.",
+    },
+    {
+        id: 5,
+        value: "Humorous",
+        description:
+            "Injects wit, puns, or jokes into your description to entertain readers and make your product memorable.",
+    },
+    {
+        id: 6,
+        value: "Persuasive",
+        description:
+            "Focuses on the unique selling points of the product and use persuasive language to convince readers that it's the best choice for them.",
+    },
+    {
+        id: 7,
+        value: "Emotional",
+        description:
+            "Taps into the reader's emotions by describing how the product will make them feel, improve their lives, or solve their problems.",
+    },
+    {
+        id: 8,
+        value: "Sensory",
+        description:
+            "Uses vivid, sensory language to help readers imagine what it's like to use the product, appealing to their senses of sight, touch, taste, smell, and sound.",
+    },
+    {
+        id: 9,
+        value: "Comparative",
+        description:
+            "Compares your product to similar products on the market, highlighting its advantages and unique features.",
+    },
+    { id: 10, value: "Custom", description: "20 letters max" },
+];
+
+export const servicesOptions = [
+    { id: 0, value: "General (Not specific to any group)" },
+    { id: 1, value: "Generation Alpha (Born 2013 onwards)" },
+    { id: 2, value: "Generation Z (Born 1997-2012)" },
+    { id: 3, value: "Millennials (Born 1981-1996)" },
+    { id: 4, value: "Generation X (Born 1965-1980)" },
+    { id: 5, value: "Baby Boomers (Born 1946-1964)" },
+    { id: 6, value: "Silent Generation (Born 1928-1945)" },
+    { id: 7, value: "New Parents" },
+    { id: 8, value: "Fitness Enthusiasts" },
+    { id: 9, value: "Pet Owners" },
+    { id: 10, value: "Tech-savvy" },
+    { id: 11, value: "Music Enthusiasts" },
+    { id: 12, value: "Sports Fans" },
+    { id: 13, value: "Soccer Moms" },
+    { id: 14, value: "Outdoor Adventures" },
+    { id: 15, value: "Wellness Seekers" },
+    { id: 16, value: "Students" },
+    { id: 17, value: "Foodies" },
+    { id: 18, value: "Travel Buffs" },
+    { id: 19, value: "Home Decor Enthusiasts" },
+    { id: 20, value: "Beautyand Skincare Aficionados" },
+    { id: 21, value: "Video Gamers" },
+    { id: 22, value: "Custom" },
+];
--- src/widget.tsx
+++ src/widget.tsx
@@ -33,6 +33,7 @@ import {
 import { RichContent } from "ricos-schema";
 import { Node, Node_Type } from "ricos-schema";
 // import { PremiumSmall } from '@wix/wix-ui-icons-common';
+import { writingOptions, servicesOptions } from "./dropdowns";
 
 export interface DashboardWidgetProps {
     onLoaded?: () => void;
@@ -45,7 +46,7 @@ export const Widget: FC = () => {
     const [remainingTokens, setRemainingTokens] = useState<number>();
     const [totalTokens, setTotalTokens] = useState<number>();
     const [genButtonLoading, setGenButtonLoading] = useState(false);
-    const [wordsNum, setWordsNum] = useState<number>(100);
+    const [wordsNum, setWordsNum] = useState<number>(500);
     const [draftName, setDraftName] = useState<string>("");
 
     useEffect(() => {
@@ -102,63 +103,7 @@ export const Widget: FC = () => {
     const [additionalInfo, setAdditionalInfo] = useState("");
     const [selectedWritingStyle, setSelectedWritingStyle] = useState<number>(1);
     const [customStyle, setCustomStyle] = useState<string>("");
-    const writingStylesOptions = [
-        {
-            id: 1,
-            value: "Conversational",
-            description:
-                "Uses a friendly, casual tone that makes the reader feel like you're speaking directly to them. This approach helps build rapport and trust with potential customers.",
-        },
-        {
-            id: 2,
-            value: "Professional",
-            description:
-                "Maintains a polished and formal tone that conveys expertise and credibility. This style is suitable for high-end or technical products where professionalism is crucial.",
-        },
-        {
-            id: 3,
-            value: "Informative",
-            description:
-                "Provides clear, concise information about the product's features, benefits, and specifications to help readers make informed purchasing decisions.",
-        },
-        {
-            id: 4,
-            value: "Storytelling",
-            description:
-                "Creates a narrative around the product, highlighting its origin, the people who created it, or the problem it solves to engage readers emotionally.",
-        },
-        {
-            id: 5,
-            value: "Humorous",
-            description:
-                "Injects wit, puns, or jokes into your description to entertain readers and make your product memorable.",
-        },
-        {
-            id: 6,
-            value: "Persuasive",
-            description:
-                "Focuses on the unique selling points of the product and use persuasive language to convince readers that it's the best choice for them.",
-        },
-        {
-            id: 7,
-            value: "Emotional",
-            description:
-                "Taps into the reader's emotions by describing how the product will make them feel, improve their lives, or solve their problems.",
-        },
-        {
-            id: 8,
-            value: "Sensory",
-            description:
-                "Uses vivid, sensory language to help readers imagine what it's like to use the product, appealing to their senses of sight, touch, taste, smell, and sound.",
-        },
-        {
-            id: 9,
-            value: "Comparative",
-            description:
-                "Compares your product to similar products on the market, highlighting its advantages and unique features.",
-        },
-        { id: 10, value: "Custom", description: "20 letters max" },
-    ];
+    const writingStylesOptions = writingOptions;
 
     const [selectedVersion, setSelectedVersion] = useState<number>(0);
     const versionOptions = [
@@ -168,31 +113,7 @@ export const Widget: FC = () => {
 
     const [customAudience, setCustomAudience] = useState<string>("");
     const [selectedOptions, setSelectedOptions] = useState<number[]>([0]);
-    const serviceOptions = [
-        { id: 0, value: "General (Not specific to any group)" },
-        { id: 1, value: "Generation Alpha (Born 2013 onwards)" },
-        { id: 2, value: "Generation Z (Born 1997-2012)" },
-        { id: 3, value: "Millennials (Born 1981-1996)" },
-        { id: 4, value: "Generation X (Born 1965-1980)" },
-        { id: 5, value: "Baby Boomers (Born 1946-1964)" },
-        { id: 6, value: "Silent Generation (Born 1928-1945)" },
-        { id: 7, value: "New Parents" },
-        { id: 8, value: "Fitness Enthusiasts" },
-        { id: 9, value: "Pet Owners" },
-        { id: 10, value: "Tech-savvy" },
-        { id: 11, value: "Music Enthusiasts" },
-        { id: 12, value: "Sports Fans" },
-        { id: 13, value: "Soccer Moms" },
-        { id: 14, value: "Outdoor Adventures" },
-        { id: 15, value: "Wellness Seekers" },
-        { id: 16, value: "Students" },
-        { id: 17, value: "Foodies" },
-        { id: 18, value: "Travel Buffs" },
-        { id: 19, value: "Home Decor Enthusiasts" },
-        { id: 20, value: "Beautyand Skincare Aficionados" },
-        { id: 21, value: "Video Gamers" },
-        { id: 22, value: "Custom" },
-    ].map((option) => ({
+    const serviceOptions = servicesOptions.map((option) => ({
         ...option,
         disabled: option.id === 0 ? selectedOptions.length > 0 : false,
     }));
@@ -267,7 +188,6 @@ export const Widget: FC = () => {
                         .split("\n\n")
                         .map((paragraph: string) => ({
                             type: Node_Type.PARAGRAPH,
-                            id: "unique_paragraph_id",
                             nodes: [
                                 {
                                     textData: {
@@ -307,7 +227,7 @@ export const Widget: FC = () => {
                     }
                 } else {
                     showToast({
-                        message: `Content successfully generated, ${data.currentTokensUsage} tokens used.`,
+                        message: "Content successfully generated.",
                         type: "success",
                     });
                 }
@@ -332,10 +252,9 @@ export const Widget: FC = () => {
         return <PageLoader />;
     }
 
-    //TODO: DO NOT FORGET TO COMMENT OUT WHEN PUSH
-    // if ((appData as any)?.instance_id === false) {
-    //     return <InstallationError />;
-    // }
+    if ((appData as any)?.instance_id === false) {
+        return <InstallationError />;
+    }
 
     return (
         <ThemeProvider theme={theme({ active: true })}>
@@ -505,7 +424,7 @@ export const Widget: FC = () => {
                                 {genButtonLoading ? (
                                     <Loader size="tiny" />
                                 ) : (
-                                    "Generate Description"
+                                    "Generate Blog Post"
                                 )}
                             </Button>
                         </Cell>
